9f7b60cccf6a3e6dd6e8434a90e40a87041e18ef
coyotte508 <coyotte508@gmail.com>
2020-06-01 14:59:47 +0200

    Add exports

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export {setup, currentPlayers, move, moveAI, stripSecret, ended} from './src/engine';
 export type {GameState, Player, Card} from './src/gamestate';
-export {MoveName} from './src/move';
+export {MoveName, Move, Moves} from './src/move';
 export {Phase} from './src/gamestate';
 export {GameEventName, LogItem} from './src/log';
 export {availableMoves, AvailableMoves} from './src/available-moves';
